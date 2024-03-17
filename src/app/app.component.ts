19c1adfdd9d65450bb040f930669af1b95203f27
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2024-03-17 16:31:47 -0300

    Revert "Add product context"
    This reverts commit a554c60ea1fce5c9e0a79c9d718ebe2c164dfc85.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductComponent } from './pages/product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,3 @@
 import { Routes } from '@angular/router';
-import { ProductComponent } from './pages/product/product.component';
 
-export const routes: Routes = [
-];
+export const routes: Routes = [];
