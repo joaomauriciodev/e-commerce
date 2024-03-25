6aadf64434acf8d777be2015c27b02e8d6df3c3e
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2024-03-25 19:44:37 -0300

    Add Side Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,22 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FormNewAdComponent } from './pages/form-new-ad/form-new-ad.component';
 import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     FormNewAdComponent, 
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
     RouterOutlet, 
     RouterLink, 
     RouterLinkActive,
@@ -16,5 +26,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'e-commerce';
+  isMenuOpen: boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
