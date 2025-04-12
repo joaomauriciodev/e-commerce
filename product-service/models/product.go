7d8c23eabbf3c09f3161982036254a7b90a8f43a
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2025-04-12 12:44:19 -0300

    fix: resolve bad syntax

--- product-service/models/product.go
+++ product-service/models/product.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Product struct {
-	ID          primitive.ObjectID `bson: "_id,omitempty" json: "id"`
-	Name        string             `bson: "name" json: "name"`
-	Description string             `bson: "description" json: "description"`
-	Price       float64            `bson: "price" json: "price" `
-	Invetory    int                `bson: "inventory" json: "inventory"`
-	CreatedAt   time.Time          `bson: "created_at json: "created_at"`
-	UpdatedAt   time.Time          `bson: "updated_at json: "updated_at"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name        string             `bson:"name" json:"name"`
+	Description string             `bson:"description" json:"description"`
+	Price       float64            `bson:"price" json:"price" `
+	Invetory    int                `bson:"inventory" json:"inventory"`
+	CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
 }
