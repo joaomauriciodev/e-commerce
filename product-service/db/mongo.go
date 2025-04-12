87f5e960b1796718e432bb5f85de67d44018cd02
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2025-04-12 13:08:59 -0300

    feat: config db mongodb

---
+++ product-service/db/mongo.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoDb struct {
+	client     *mongo.Client
+	collection *mongo.Collection
+}
+
+func Connect(uri string) (*MongoDb, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	collection := client.Database("ecommerce").Collection("prodcuts")
+
+	return &MongoDb{
+		client:     client,
+		collection: collection,
+	}, nil
+}
--- product-service/main.go
+++ product-service/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"joaomauriciodev/e-commerce/db"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Helloooo")
+	mongoURI := os.Getenv("MONGO_URL")
+	if mongoURI == "" {
+		mongoURI = "mongodb://localhost:27017"
+	}
+
+	_, err := db.Connect(mongoURI)
+	if err != nil {
+		log.Fatalf("Falha ao conectar ao banco de dados: %v", err)
+	}
+
 }
