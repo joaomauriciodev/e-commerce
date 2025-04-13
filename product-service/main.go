8b5de6f24182cdbaca5bdc0f3fb86f721d9a3cee
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2025-04-13 10:35:36 -0300

    feat: implemets product routes

--- product-service/main.go
+++ product-service/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"joaomauriciodev/e-commerce/api"
 	"joaomauriciodev/e-commerce/db"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -12,9 +14,20 @@ func main() {
 		mongoURI = "mongodb://localhost:27017"
 	}
 
-	_, err := db.Connect(mongoURI)
+	database, err := db.Connect(mongoURI)
 	if err != nil {
 		log.Fatalf("Falha ao conectar ao banco de dados: %v", err)
 	}
 
+	router := api.SetupRoutes(database)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+
+	log.Printf("Serviço de Produtos rodando na porta %s", port)
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	}
 }
